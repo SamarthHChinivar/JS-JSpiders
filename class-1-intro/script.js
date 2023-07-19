679b302e224d0128b04e8761aed4e8c90f29106c
Samarth H Chinivar <samarthharish321@gmail.com>
2023-07-19 20:35:00 +0530

    class-1 commit

---
+++ class-1-intro/script.js
@@ -0,0 +1,25 @@
+console.log("hello");
+
+var str="ABD";
+console.log(str);
+console.log(typeof str);
+
+var str = -7.887
+console.log(str);
+console.log(typeof str);
+
+var str=`skjc`;
+console.log(str);
+console.log(typeof str);
+
+var str=true;
+console.log(str);
+console.log(typeof str);
+
+var str=
+console.log(str);
+console.log(typeof str);
+
+var str= null ;
+console.log(str);
+console.log(typeof str);
